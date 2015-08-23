f6b22ba35ee891634e20965c0fc2522c3fe0ba51
rroggenbuck <robert.roggenbuck@wiethe.com>
2015-08-23 14:25:34 +0200

    Added basic infrastructure to collect treasures and grouping of visitors. Added methods groupSize(), findNearestVisitor() and setSpriteOrientation(). Extended update() for group building.

--- library/visitor.js
+++ library/visitor.js
@@ -11,7 +11,36 @@ Visitor = function (game, sprite) {
     this.game = game;
     this.sprite = sprite;
     this.body = this.sprite.body;
-}
+    this.treasures = 0;
+    this.groupsize = 1;
+
+    /**
+     * 1 = north
+     * 2 = north-east
+     * 3 = east
+     * ...
+     * 8 = north-west
+     *
+     * @type {number} rotationIndex
+     */
+    this.rotationIndex = 1;
+};
+
+var maxGroupSize = 6;
+
+/**
+ * Setter / getter for the group size.
+ *
+ * @param size
+ * @returns {number}
+ */
+Visitor.prototype.groupSize = function (size) {
+    if (size) {
+        this.groupsize = size;
+    }
+
+    return this.groupsize;
+};
 
 
 /**
@@ -22,7 +51,7 @@ Visitor = function (game, sprite) {
  */
 Visitor.prototype.blocked = function () {
     return this.body.blocked.up || this.body.blocked.down || this.body.blocked.left || this.body.blocked.right;
-}
+};
 
 /**
  * Absconds from Minotaur into reverse direction accelerated by factor 10
@@ -32,7 +61,7 @@ Visitor.prototype.blocked = function () {
  */
 Visitor.prototype.flee = function (minotaur) {
     this.changeDirection(Phaser.Point.angle(this.body.position, minotaur.body.position), 10);
-}
+};
 
 /**
  * Starts walking randomly around
@@ -41,8 +70,7 @@ Visitor.prototype.flee = function (minotaur) {
  */
 Visitor.prototype.startWalking = function () {
     this.changeDirection(Math.random() * Math.PI * 2);
-
-}
+};
 
 /**
  * Changes the Visitor's direction relative to the given target. Or changes
@@ -62,7 +90,7 @@ Visitor.prototype.changeDirection = function (targetOrAngle, acceleration) {
     }
     this.body.velocity.y = visitorSpeed * Math.sin(targetOrAngle) * acceleration;
     this.body.velocity.x = visitorSpeed * Math.cos(targetOrAngle) * acceleration;
-}
+};
 
 /**
  * Grabs treasure
@@ -71,8 +99,9 @@ Visitor.prototype.changeDirection = function (targetOrAngle, acceleration) {
  * @param {Treasure} treasure - The treasure to chase and grab
  */
 Visitor.prototype.grab = function (treasure) {
-
-}
+    this.treasures.push(treasure);
+    treasure.visible = false;
+};
 
 /**
  * Loops the treasure collection. Returns the nearest treasure found
@@ -93,7 +122,28 @@ Visitor.prototype.findNearestTreasure = function (treasures) {
         }
     }
     return nearestTreasure;
-}
+};
+
+/**
+ * Loops the treasure collection. Returns the nearest treasure found
+ *
+ * @method Visitor#findNearestVisitor
+ * @param {Array.<Treasure>} visitors - The visitor collection
+ * @return {Treasure|undefined} Nearest treasure found
+ */
+Visitor.prototype.findNearestVisitor = function (visitors) {
+    var maxRange = 50;
+    var shortestDistance = 0;
+    var nearestTreasure;
+    for (var idx in visitors) {
+        var distance = Phaser.Point.distance(this.body.position, visitors[idx].body.position, 0);
+        if (distance < maxRange && (!shortestDistance || distance < shortestDistance)) {
+            nearestTreasure = visitors[idx];
+            shortestDistance = distance;
+        }
+    }
+    return nearestTreasure;
+};
 
 /**
  * Updates the Visitor each cycle. Contains the Visitor's KI-circuits.
@@ -105,10 +155,18 @@ Visitor.prototype.findNearestTreasure = function (treasures) {
 Visitor.prototype.update = function (minotaur, treasures) {
     this.game.physics.arcade.collide(this.sprite, wallsLayer);
     this.game.physics.arcade.collide(this.sprite, decorationLayer);
-    var seesMinotaur = Phaser.Point.distance(this.body.position, minotaur.body.position, 0) < 200;
-    var isMoving = this.body.velocity.x || this.body.velocity.y;
-    var blocked = this.blocked();
+
+    var seesMinotaur = Phaser.Point.distance(this.body.position, minotaur.body.position, 0) < iCanSeeYouDistance;
+    var isMoving     = this.body.velocity.x || this.body.velocity.y;
+    var blocked      = this.blocked();
     var foundTreasure = this.findNearestTreasure(treasures);
+
+    var seesTreasure = false;
+    if (typeof foundTreasure != 'undefined') {
+        seesTreasure = Phaser.Point.distance(this.body.position, foundTreasure.body.position, 0) < iCanSeeYouDistance;
+    }
+
+    var foundVisitor = this.findNearestVisitor(visitors);
 
     switch (true) {
         case (seesMinotaur):
@@ -120,15 +178,33 @@ Visitor.prototype.update = function (minotaur, treasures) {
                 this.grab(foundTreasure);
                 break;
             }
-        case (foundTreasure):
+        case (seesTreasure):
             this.changeDirection(foundTreasure.body.position);
+            break;
+        case (foundVisitor):
+            var meetVisitor = Phaser.Point.distance(this.body.position, foundVisitor.body.position, 0) < 5;
+            if (meetVisitor) {
+                this.groupsize += 1;
+
+                if (this.groupSize() > maxGroupSize) {
+                    this.splitGroup();
+                }
+
+                // select a new image for the current view-direction
+                this.sprite.frame = this.groupsize + 10 * this.rotationIndex;
+
+                // transfer the treasures
+                groupTreasures = this.treasures.concat(foundVisitor.treasures);
+                this.treasures = groupTreasures;
+                foundVisitor.destroy();
+            }
             break;
         case (blocked):
         case (!isMoving):
             this.startWalking();
         default:
     }
-}
+};
 
 /**
  * Creates and returns a new visitor. Consumes an 'Game Object' with
@@ -153,4 +229,47 @@ Visitor.create = function (game, gameObject) {
     sprite.body.collideWorldBounds = true;
 
     return new Visitor(game, sprite);
-}
+};
+
+/**
+ * Sets the orientation of the visitor image.
+ * Takes an orientation short cut as argument:
+ * n, ne, e, ...
+ *
+ * @param {string} skyDir
+ * @returns {number}
+ */
+Visitor.prototype.setSpriteOrientation = function (orientation) {
+    switch (orientation) {
+        case ('n'):
+            this.rotationIndex = 1;
+            break;
+        case ('ne'):
+            this.rotationIndex = 2;
+            break;
+        case ('e'):
+            this.rotationIndex = 3;
+            break;
+        case ('se'):
+            this.rotationIndex = 4;
+            break;
+        case ('s'):
+            this.rotationIndex = 5;
+            break;
+        case ('sw'):
+            this.rotationIndex = 6;
+            break;
+        case ('w'):
+            this.rotationIndex = 7;
+            break;
+        case ('nw'):
+            this.rotationIndex = 8;
+            break;
+        default:
+            this.rotationIndex = 1;
+            break;
+    }
+    return this.rotationIndex;
+};
+
+
