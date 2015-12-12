b569be73ec46b1381687ff210442be97c3a37972
Patrick Florek <patrick.florek@wiethe.com>
2015-12-12 17:53:34 +0100

    add plane

--- library/player.js
+++ library/player.js
@@ -1,11 +1,15 @@
 var Player = function (app) {
     this.app = app
+    this.plane
 }
 
 Player.prototype.preload = function () {
+    this.app.game.load.spritesheet('plane', 'assets/images/player/plane.png', 128, 128)
 }
 
 Player.prototype.create = function () {
+    this.plane = this.app.game.add.sprite(0, 0, 'plane');
+    this.app.game.physics.arcade.enable(this.plane);
 }
 
 Player.prototype.update = function () {
