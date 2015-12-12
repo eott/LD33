01a63568194d9e109bbf01c0f7c2316d0ee10fd2
Anton Mastel <anton.mastel@wiethe.com>
2015-12-12 19:17:20 +0100

    stuff

--- library/player.js
+++ library/player.js
@@ -11,9 +11,28 @@ Player.prototype.create = function () {
     this.plane = this.app.game.add.sprite(0, 0, 'plane')
     this.app.game.physics.arcade.enable(this.plane)
     this.plane.checkWorldBounds = true
-    this.plane.events.onOutOfBounds.add(function(){console.log('collision'), this.plane})
-    this.plane.body.velocity.x = 1000
-    this.plane.body.velocity.y = 1000
+
+
+    this.plane.events.onOutOfBounds.add(this.onOutOfBounds, this)
+
+
+
+    this.plane.body.velocity.x = 100
+    this.plane.body.velocity.y = 100
+}
+
+Player.prototype.onOutOfBounds = function(){
+    console.log('collision')
+    console.log(this.app.game.height)
+    console.log(this.plane.body.position.y)
+
+    if (this.app.game.height - this.plane.body.position.y <= 0){
+        this.plane.body.position.y = 0
+    }
+
+    console.log(this.app.game.height)
+    console.log(this.plane.body.position.y)
+
 }
 
 Player.prototype.update = function () {
