ff9b6e0a50a7c73167b77bf8bf2c7b9c5ddf8442
Patrick Florek <patrick.florek@wiethe.com>
2015-12-12 15:01:30 +0100

    SFX prot

--- library/sfx.js
+++ library/sfx.js
@@ -1,81 +1,49 @@
-var sfx = (function () {
-    var app;
-    var muted = false;
-    var backgroundMusic;
-    var audioClips = {};
+var SFX = function (app) {
+    this.app = app
+    this.muted = false
+    this.backgroundMusic
+    this.audioClips = {}
+}
 
-    function load(app) {
-        this.app = app;
-        this.app.game.load.audio('music', 'assets/audio/music/song.ogg');
-        this.app.game.load.audio('steps', 'assets/audio/effects/footsteps.ogg');
-        this.app.game.load.audio('pickup', 'assets/audio/effects/pickup.ogg');
+SFX.prototype.load = function () {
+}
+
+SFX.prototype.init = function () {
+}
+
+SFX.prototype.update = function () {
+}
+
+SFX.prototype.mute = function () {
+    this.muted = !this.muted
+
+    if (!this.muted) {
+        document.getElementById("mute").style.display = "inline-block"
+        document.getElementById("unmute").style.display = "none"
+        this.backgroundMusic.play()
+    } else {
+        document.getElementById("mute").style.display = "none"
+        document.getElementById("unmute").style.display = "inline-block"
+        this.backgroundMusic.pause()
+    }
+}
+
+SFX.prototype.play = function (name) {
+    if (this.muted) {
+        return
     }
 
-    function init() {
-        backgroundMusic = app.game.add.audio('music', 0.3, true);
-        audioClips = {
-            'steps' : app.game.add.audio('steps', 0.4),
-            'pickup': app.game.add.audio('pickup', 0.1)
-        }
-
-        audioClips['steps'].loop = true;
-
-        play('backgroundMusic');
+    if (name == 'backgroundMusic') {
+        this.backgroundMusic.play()
+    } else if (this.audioClips[name] && !this.audioClips[name].isPlaying) {
+        this.audioClips[name].play()
     }
+}
 
-    function update() {
-        if (
-            app.player.body.velocity.y != 0
-                || app.player.body.velocity.x != 0
-            ) {
-            play('steps');
-        } else {
-            pause('steps');
-        }
+SFX.prototype.pause = function (name) {
+    if (name == 'backgroundMusic') {
+        this.backgroundMusic.pause()
+    } else if (audioClips[name]) {
+        this.audioClips[name].pause()
     }
-
-    function mute() {
-        muted = !muted;
-
-        if (!muted) {
-            document.getElementById("mute").style.display = "inline-block";
-            document.getElementById("unmute").style.display = "none";
-            backgroundMusic.play();
-        } else {
-            document.getElementById("mute").style.display = "none";
-            document.getElementById("unmute").style.display = "inline-block";
-            backgroundMusic.pause();
-        }
-    }
-
-    function play(name) {
-        if (muted) {
-            return;
-        }
-
-        if (name == 'backgroundMusic') {
-            backgroundMusic.play();
-        } else if (audioClips[name] && !audioClips[name].isPlaying) {
-            audioClips[name].play();
-        }
-    }
-
-    function pause(name) {
-        if (name == 'backgroundMusic') {
-            backgroundMusic.pause();
-        } else if (audioClips[name]) {
-            audioClips[name].pause();
-        }
-    }
-
-
-    return {
-        load  : load,
-        init  : init,
-        update: update,
-        mute  : mute,
-        play  : play,
-        pause : pause
-    };
-
-})();
+}
