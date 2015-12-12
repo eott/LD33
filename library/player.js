0be2ad4592654af7f7a73f2372ef19fb539e4533
Patrick Florek <patrick.florek@wiethe.com>
2015-12-12 21:02:35 +0100

    Implement the controls left and right

--- library/player.js
+++ library/player.js
@@ -1,28 +1,27 @@
 var Player = function (app) {
     this.app = app
     this.plane
+    this.speed = 100
+    this.incr = 32
+    this.dimension = new Phaser.Point(128, 128)
 }
 
 Player.prototype.preload = function () {
-    this.app.game.load.spritesheet('plane', 'assets/images/player/plane.png', 128, 128)
+    this.app.game.load.spritesheet('plane', 'assets/images/player/plane.png', this.dimension.x, this.dimension.y)
 }
 
 Player.prototype.create = function () {
-    this.plane = this.app.game.add.sprite(0, 0, 'plane')
+    this.plane = this.app.game.add.sprite(300, 300, 'plane')
     this.app.game.physics.arcade.enable(this.plane)
     this.plane.checkWorldBounds = true
-
-
-
     this.plane.events.onOutOfBounds.add(this.onOutOfBounds, this)
 
-
-
-    this.plane.body.velocity.x = -1000
-    this.plane.body.velocity.y = -1000
+    var angle = Phaser.Point.angle(new Phaser.Point(0, 0), new Phaser.Point(1 * this.speed, 1 * this.speed))
+    this.plane.body.velocity.y = Math.sin(angle) * this.speed
+    this.plane.body.velocity.x = Math.cos(angle) * this.speed
 }
 
-Player.prototype.onOutOfBounds = function(){
+Player.prototype.onOutOfBounds = function () {
     console.log('collision')
 
     if (this.plane.position.y >= this.app.game.height) {
@@ -40,22 +39,22 @@ Player.prototype.onOutOfBounds = function(){
 
 Player.prototype.update = function () {
 
-    this.plane.rotation = this.plane.body.angle + Math.PI/2;
+    // Rotation for sprite
+    this.plane.rotation = this.plane.body.angle + Math.PI / 2
 
-    console.log(this.plane.rotation)
+    //Direction for velocity
+    var angle = Phaser.Point.angle(this.plane.body.velocity, new Phaser.Point(0, 0))
 
-    console.log(Phaser.Point.angle(new Phaser.Point(0, 0), this.plane.body.velocity))
-
-    if (this.app.cursors.left.isDown)
-    {
-        this.plane.body.velocity.y
+    if (this.app.cursors.left.isDown) {
+        angle -= Math.PI / this.incr
     }
 
-    if (this.app.cursors.right.isDown)
-    {
-        this.plane.body.velocity.y
+    if (this.app.cursors.right.isDown) {
+        angle += Math.PI / this.incr
     }
 
+    this.plane.body.velocity.y = Math.sin(angle) * this.speed
+    this.plane.body.velocity.x = Math.cos(angle) * this.speed
 }
 
 Player.prototype.reset = function () {
