882d151f4b2562a6da307697d30ac3366563c7ca
Patrick Florek <patrick.florek@wiethe.com>
2015-12-12 14:51:03 +0100

    constructor with prototypes

--- library/player.js
+++ library/player.js
@@ -1,21 +1,21 @@
 var Player = function (app) {
     this.app = app
+}
 
-    this.load = function () {
-        self.app = app;
-    }
+Player.prototype.load = function () {
+    self.app = app;
+}
 
-    this.init = function () {
-        // Camera and game world
-        app.game.camera.follow(player.sprite);
-        app.game.world.setBounds(0, 0, 2500, 2500);
-    }
+Player.prototype.init = function () {
+    // Camera and game world
+    app.game.camera.follow(player.sprite);
+    app.game.world.setBounds(0, 0, 2500, 2500);
+}
 
-    this.update = function () {
+Player.prototype.update = function () {
 
-    }
+}
 
-    this.reset = function () {
+Player.prototype.reset = function () {
 
-    }
 }
