7b9fd8dc2f649a921da6c62a5ea735d114a44866
Patrick Florek <patrick.florek@gmail.com>
2015-12-13 13:05:04 +0100

    Implements preloading of powerup sprites

--- library/powerupdistributor.js
+++ library/powerupdistributor.js
@@ -4,7 +4,10 @@ var PowerupDistibutor = function (app) {
 }
 
 Powerup.prototype.preload = function () {
-
+    for (var value of ['ballon', 'speed', 'spray'])
+    {
+        this.app.game.load.spritesheet(value, 'assets/images/objects/' + value + '.png', 32, 32)
+    }
 }
 
 Powerup.prototype.create = function () {
