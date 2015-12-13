c8dff86ba108bdb3b96325ead2c72fa760eb7dab
Patrick Florek <patrick.florek@gmail.com>
2015-12-13 12:36:51 +0100

    distributor skeleton

---
+++ library/powerupdistributor.js
@@ -0,0 +1,16 @@
+var PowerupDistibutor = function (app) {
+    this.app = app
+    this.powerups = {}
+}
+
+Powerup.prototype.preload = function () {
+
+}
+
+Powerup.prototype.create = function () {
+
+}
+
+Powerup.prototype.update = function () {
+
+}
