c5698ff65a101ddd8646f32d54504ca7d354d51e
Patrick Florek <patrick.florek@gmail.com>
2015-12-13 18:48:22 +0100

    Tests for powerup collision and removes the powerup
    @todo the powerup logic

--- library/powerups.js
+++ library/powerups.js
@@ -6,8 +6,7 @@ var Powerups = function (app) {
 }
 
 Powerups.prototype.preload = function () {
-    for (var value of this.types)
-    {
+    for (var value of this.types) {
         this.app.game.load.spritesheet(value, 'assets/images/objects/' + value + '.png', 32, 32)
     }
 }
@@ -15,19 +14,31 @@ Powerups.prototype.preload = function () {
 Powerups.prototype.create = function () {
     var sprite
     while (this.powerups.length < this.maxPowerups) {
-        sprite = this.app.add.sprite(Math.random() * this.app.game.width, Math.random() * this.app.game.height, this.types[Math.floor(Math.random() * this.types.length)])
+        sprite = this.app.game.add.sprite(Math.random() * this.app.game.width, Math.random() * this.app.game.height, this.types[Math.floor(Math.random() * this.types.length)])
+        this.app.game.physics.arcade.enable(sprite)
+        sprite.app = this.app
         this.powerups.push(sprite)
-        this.app.game.physics.arcade.collide(
-            this.app.player.plane,
-            sprite,
-            function () {
-                console.log('power up collected')
-            },
-            null,
-            this)
     }
 }
 
 Powerups.prototype.update = function () {
+    for (var i in this.powerups) {
+        if(this.app.game.physics.arcade.collide(this.app.player.plane, this.powerups[i])) {
+            this.handleCollision(i)
+        }
+    }
+}
 
+Powerups.prototype.handleCollision = function (index) {
+    console.log('Powerup collected')
+    var sprite = this.powerups[index]
+    var key = sprite.key
+
+    // Fancy collection animation
+
+    // Remove sprite
+    sprite.kill()
+    this.powerups.splice(index, 1)
+
+    // Apply powerup bonusses
 }
